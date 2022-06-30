62810c5fc9fa73c3c060567e0571989176442a1a
Academic7003 <nazarmamatovnurmuhammad@gmail.com>
2022-06-30 17:49:01 +0500

    heroku

--- corebook/settings.py
+++ corebook/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-3r#$%01i+031mqvoq)=*8%b%%tk=5_1cb-a0*txgu9i-vt7+v7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
